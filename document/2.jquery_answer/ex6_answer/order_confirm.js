6168b5b6044da90b04a54cee1704b2fbd4cf554f
igamasayuki <igamasayuki@gmail.com>
2022-06-02 15:56:18 +0900

    コードフォーマットby Prettier

--- document/2.jquery_answer/ex6_answer/order_confirm.js
+++ document/2.jquery_answer/ex6_answer/order_confirm.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 $(function () {
-	$('#address').on('click', function () {
-		AjaxZip3.zip2addr('zip11', '', 'addr11', 'addr11');
-	});
+  $("#address").on("click", function () {
+    AjaxZip3.zip2addr("zip11", "", "addr11", "addr11");
+  });
 });
