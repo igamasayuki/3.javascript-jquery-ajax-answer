295ebdb044f6599d930ba85ef7c498e8bf894f1c
igamasayuki <igamasayuki@gmail.com>
2025-05-30 14:04:30 +0900

    functionをアロー関数へ変更＆jQueryバージョンを最新化

--- document/2.jquery_sample/sample_html_operation.js
+++ document/2.jquery_sample/sample_html_operation.js
@@ -1,9 +1,9 @@
 "use strict";
 
 let imgRotate = 0; // サンプル2で使用グローバル変数(回転角度)
-$(function () {
+$(() => {
   // サンプル1(現在の日付をテキストボックスに入れる)
-  $(document).on("click", "#sample1button", function () {
+  $(document).on("click", "#sample1button", () => {
     // ボタン押下時
     $("#nowText").val(new Date());
   });
@@ -11,10 +11,10 @@ $(function () {
   // サンプル2 (テキストの色の入れ替え＆画像回転)
   $("#blueOrRedText").css("color", "blue"); // 初期表示時は青
 
-  $(document).on("click", "#sample2button", function () {
+  $(document).on("click", "#sample2button", () => {
     // ボタン押下時
     // 色の入れ替え
-    var targetText = $("#blueOrRedText").css("color");
+    const targetText = $("#blueOrRedText").css("color");
     if (targetText === "rgb(0, 0, 255)") {
       // 青なら赤へ、赤なら青へ
       $("#blueOrRedText").css("color", "red");
@@ -34,7 +34,7 @@ $(function () {
   // $('#triathlon').hide(); //hide()関数でも非表示になる
   // $('#other').hide();
 
-  $(document).on("change", "#sample3select", function () {
+  $(document).on("change", "#sample3select", () => {
     // 選択肢変更時
     if ($("#sample3select").val() === "1") {
       $("#triathlon").show();
@@ -48,7 +48,7 @@ $(function () {
     }
   });
 
-  //	$('#sample3select').on('change', function() { // 選択肢変更時
+  //	$('#sample3select').on('change', () => { // 選択肢変更時
   //		if ($('#sample3select').val() == '1') {
   //			$('#triathlon').css('display', 'block');
   //			$('#other').css('display', 'none');
@@ -62,7 +62,7 @@ $(function () {
   //	});
 
   // サンプル4(フォーム送信ボタンを押した時、送信前に入力内容をチェックする)
-  $(document).on("click", "#sample4Button", function () {
+  $(document).on("click", "#sample4Button", () => {
     // ボタン押下時
     var nameValue = $("#name").val();
     if (nameValue === "") {
@@ -74,13 +74,13 @@ $(function () {
   });
 
   // サンプル5 (ダブルサブミット対策:1度押したら2度目を押せなくする)
-  $(document).on("click", "#sample5Button", function () {
+  $(document).on("click", "#sample5Button", () => {
     // ボタン押下時
     $("#sample5Button").prop("disabled", true);
   });
 
   // サンプル6 (ダミーフォームを作りボタンを横に並べる)
-  $(document).on("click", "#sample6Button", function () {
+  $(document).on("click", "#sample6Button", () => {
     // ボタン押下時
     alert("投稿ではなく削除のダミーフォームをサブミット！");
     // sample6Formというidがつけられたフォームをサブミットする(サーバーへ送る)
